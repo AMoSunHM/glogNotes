04e5d6fe1cec358df5ea78325d20c2e155b1621d
casicApollo <373915439@qq.com>
2022-08-09 15:39:18 +0800

    1st demo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # glogNotes
+
+Inspired by Apollo.
+
+It can be a substitute for std::cout.
---
+++ demo/main.cc
@@ -0,0 +1,12 @@
+//#include <glog/logging.h>
+#include "log/log.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    //google::InitGoogleLogging(argv[0]);
+    //LOG(INFO) << "Found " <<" cookies";
+    //INFO <<"AA";
+    AINFO << "FOUND";
+    return 0;
+}
